00b6bc0eece911c9a9681972f538f8b861ca250b
jlangr <jeff@langrsoft.com>
2013-05-10 10:06:48 -0600

    fix

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -1,9 +1,10 @@
-#include "gmock/gmock.h"
-
 // START_HIGHLIGHT
 class Soundex {
 };
 // END_HIGHLIGHT
+
+
+#include "gmock/gmock.h"
 
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
    Soundex soundex;
