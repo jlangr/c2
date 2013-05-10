ac905b0f0503de3abc9fbac5308d90e63893c5a3
jlangr <jeff@langrsoft.com>
2013-05-10 09:33:38 -0600

    9

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -1,4 +1,4 @@
-#include "gmock/gmock.h"    
+#include "gmock/gmock.h" 
 
 #include <string>
 
@@ -6,23 +6,36 @@
 class Soundex
 {
 public:
+// START:encode
    std::string encode(const std::string& word) const {
 // START_HIGHLIGHT
-      return word;
+      return word + "000";
 // END_HIGHLIGHT
    }
+// END:encode
 };
 // END:Soundex
 
-// START:test
 using ::testing::Eq;
 
+// START:RetainsSoleLetter
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
    Soundex soundex;
    
    auto encoded = soundex.encode("A");
 
-   ASSERT_THAT(encoded, Eq("A")); 
+// START_HIGHLIGHT
+   ASSERT_THAT(encoded, Eq("A000")); 
+// END_HIGHLIGHT
 }
-// END:test
+// END:RetainsSoleLetter
 
+// START:PadsWithZeros
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+   Soundex soundex;
+
+   auto encoded = soundex.encode("I");
+
+   ASSERT_THAT(encoded, Eq("I000"));
+}
+// END:PadsWithZeros
