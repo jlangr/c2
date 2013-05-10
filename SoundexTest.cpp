3f5b64105359b5c889fea84ca3ef0e10604291cb
jlangr <jeff@langrsoft.com>
2013-05-10 10:03:58 -0600

    fix

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -1,5 +1,3 @@
-#include "gmock/gmock.h" 
-
 #include <string>
 
 // START:Soundex
@@ -15,6 +13,9 @@ public:
 // END:encode
 };
 // END:Soundex
+
+
+#include "gmock/gmock.h" 
 
 using ::testing::Eq;
 
