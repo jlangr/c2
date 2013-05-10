f4059035ee640acd39e3ba6ec5c00f3bd8062687
jlangr <jeff@langrsoft.com>
2013-05-10 08:52:20 -0600

    1

---
+++ SoundexTest.cpp
@@ -0,0 +1,6 @@
+#include "gmock/gmock.h"    //<label id="code.include"/>
+
+TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) { //<label id="code.test"/>
+   Soundex soundex;   //<label id="code.construct"/>
+}
+
