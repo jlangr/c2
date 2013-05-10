5438bd04014846d4a4d4c4e214793fdb9448a2f8
jlangr <jeff@langrsoft.com>
2013-05-10 09:02:52 -0600

    3

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -1,11 +1,17 @@
-#include "gmock/gmock.h"
+#include "gmock/gmock.h" 
 
 // START_HIGHLIGHT
 class Soundex {
 };
 // END_HIGHLIGHT
 
+// START:test
 TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
    Soundex soundex;
+   
+// START_HIGHLIGHT
+   auto encoded = soundex.encode("A");
+// END_HIGHLIGHT
 }
+// END:test
 
