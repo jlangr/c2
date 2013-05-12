24ad67bed820dc292fb549344ac5943f026ee9e1
jlangr <jeff@langrsoft.com>
2013-05-12 17:07:02 -0600

    21 fix

--- Soundex.h
+++ Soundex.h
@@ -29,16 +29,19 @@ private:
 
 // START:encodedDigits
    std::string encodedDigit(char letter) const {
-// START_HIGHLIGHT
       const std::unordered_map<char, std::string> encodings {
          {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+         // ...
+// END:encodedDigits
          {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"},
                      {'s', "2"}, {'x', "2"}, {'z', "2"},
          {'d', "3"}, {'t', "3"},
          {'l', "4"},
          {'m', "5"}, {'n', "5"},
          {'r', "6"}
+// START:encodedDigits
       };
+// START_HIGHLIGHT
       auto it = encodings.find(letter);
       return it == encodings.end() ? "" : it->second;
 // END_HIGHLIGHT
