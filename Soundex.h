11bb1dbac669a3b4ed24d82d9a7fd4247fa852ee
jlangr <jeff@langrsoft.com>
2013-05-13 19:43:43 -0600

    31 CombinesDuplicateEncodings

--- Soundex.h
+++ Soundex.h
@@ -26,28 +26,29 @@ private:
    std::string encodedDigits(const std::string& word) const {
       std::string encoding;
       for (auto letter: word) {
-// START_HIGHLIGHT
          if (isComplete(encoding)) break;
+// START_HIGHLIGHT
+         if (encodedDigit(letter) != lastDigit(encoding))
 // END_HIGHLIGHT
-         encoding += encodedDigit(letter);
+            encoding += encodedDigit(letter);
       }
       return encoding;
    }
 
 // START_HIGHLIGHT
-   bool isComplete (const std::string& encoding) const {
-      return encoding.length() == MaxCodeLength - 1; 
+   std::string lastDigit(const std::string& encoding) const {
+      if (encoding.empty()) return "";
+      return std::string(1, encoding.back());
    }
 // END_HIGHLIGHT
 // END:encodedDigits
 
-// START:encodedDigit
-// START_HIGHLIGHT
+   bool isComplete (const std::string& encoding) const {
+      return encoding.length() == MaxCodeLength - 1; 
+   }
+
 public:
-// END_HIGHLIGHT
    std::string encodedDigit(char letter) const {
-      // ...
-// END:encodedDigit
       const std::unordered_map<char, std::string> encodings {
          {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
          {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"},
@@ -59,14 +60,9 @@ public:
       };
       auto it = encodings.find(letter);
       return it == encodings.end() ? "" : it->second;
-// START:encodedDigit
    }
 
-// START_HIGHLIGHT
 private:
-// END_HIGHLIGHT
-   // ...
-// END:encodedDigit
    std::string zeroPad(const std::string& word) const {
       auto zerosNeeded = MaxCodeLength - word.length();
       return word + std::string(zerosNeeded, '0');
