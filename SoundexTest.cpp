bc7c3dbca60983e9d343dad8da3d56a7c1d22a90
jlangr <jeff@langrsoft.com>
2013-05-15 19:46:57 -0600

    fix

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -34,7 +34,7 @@ TEST_F(SoundexEncoding, LimitsLengthToFourCharacters) {
 }
 
 TEST_F(SoundexEncoding, IgnoresVowelLikeLetters) {
-   ASSERT_THAT(soundex.encode("Caeiouhycdl"), Eq("C234"));
+   ASSERT_THAT(soundex.encode("Baeiouhycdl"), Eq("B234"));
 }
 
 TEST_F(SoundexEncoding, CombinesDuplicateEncodings) {
